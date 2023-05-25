a8a56ba3d2ffbf7bf29a2d05052ead4d5ce88d39
Reid Mason <maddogshain132@gmail.com>
2023-05-25 17:49:15 +0100

    Initial commit

---
+++ Source/ProjectHellHound/ProjectHellHoundGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectHellHoundGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTHELLHOUND_API AProjectHellHoundGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
